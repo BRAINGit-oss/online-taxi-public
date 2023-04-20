a1906fc7dd1f025ee76f914555af34b653e50b6b
BRAINGit-oss <1834733454@qq.com>
2023-04-20 15:23:09 +0800

    校验验证吗骨架编写及校验验证码代码编写

--- api-passenger/src/main/java/com/brain/apipassenger/controller/VerificationCodeController.java
+++ api-passenger/src/main/java/com/brain/apipassenger/controller/VerificationCodeController.java
@@ -5,6 +5,7 @@ import com.brain.apipassenger.service.VerificationCodeService;
 import com.brain.internalcommon.dto.ResponseResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,5 +17,14 @@ public class VerificationCodeController {
     public ResponseResult verificationCode(@RequestBody VerificationCodeDTO verificationCodeDTO){
         String passengerPhone = verificationCodeDTO.getPassengerPhone();
         return verificationCodeService.generatorCode(passengerPhone);
+    }
+
+    @PostMapping("/verification-code-check")
+    public ResponseResult checkVerificationCode(@RequestBody VerificationCodeDTO verificationCodeDTO){
+        String passengerPhone = verificationCodeDTO.getPassengerPhone();
+        String verificationCode = verificationCodeDTO.getVerificationCode();
+        System.out.println("手机号："+passengerPhone+",验证码："+verificationCode);
+
+        return verificationCodeService.checkCode(passengerPhone,verificationCode);
     }
 }
--- api-passenger/src/main/java/com/brain/apipassenger/service/VerificationCodeService.java
+++ api-passenger/src/main/java/com/brain/apipassenger/service/VerificationCodeService.java
@@ -1,9 +1,13 @@
 package com.brain.apipassenger.service;
 
 import com.brain.apipassenger.remote.ServiceVerificationcodeClient;
+import com.brain.internalcommon.constant.CommonStatusEnum;
 import com.brain.internalcommon.dto.ResponseResult;
 import com.brain.internalcommon.response.NumberCodeResponse;
+import com.brain.internalcommon.response.TokenResponse;
+import io.netty.util.internal.StringUtil;
 import net.sf.json.JSONObject;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
@@ -21,6 +25,11 @@ public class VerificationCodeService {
     @Autowired
     private StringRedisTemplate stringRedisTemplate; //字符串类型的
 
+    /**
+     * 生成验证码
+     * @param passengerPhone 手机号
+     * @return
+     */
     public ResponseResult generatorCode(String passengerPhone){
         //调用验证码服务，获取验证码
 
@@ -29,7 +38,7 @@ public class VerificationCodeService {
         System.out.println("remote number code:"+numberCode);
 
         // key,value,ttl过期时间
-        String key = verificationCodePrefix+passengerPhone;
+        String key = generatorKeyByPhone(passengerPhone);
         //存入redis
         stringRedisTemplate.opsForValue().set(key, numberCode+"",2, TimeUnit.MINUTES);
 
@@ -40,6 +49,44 @@ public class VerificationCodeService {
 //        result.put("code",1);
 //        result.put("message","success");
         return ResponseResult.success("");
+    }
+
+    private String generatorKeyByPhone(String passengerPhone){
+        return verificationCodePrefix + passengerPhone;
+    }
+
+    /**
+     * 校验验证码
+     * @param passengerPhone 手机号
+     * @param verificationCode 验证码
+     * @return
+     */
+    public ResponseResult checkCode(String passengerPhone,String verificationCode){
+        //根据手机号，去redis读取验证码
+        //生成key
+        String key = generatorKeyByPhone(passengerPhone);
+        //根据key获取value
+        String codeRedis = stringRedisTemplate.opsForValue().get(key);
+        System.out.println("redis中的value:"+codeRedis);
+
+        //校验验证码 key未过期 key已过期 提示：验证码已过期
+        if(StringUtils.isBlank(codeRedis)){
+            return ResponseResult.fail(CommonStatusEnum.VERIFICATION_CODE_ERROR.getCode(),CommonStatusEnum.VERIFICATION_CODE_ERROR.getValue());
+        }
+        if(!verificationCode.trim().equals(codeRedis.trim())){
+            return ResponseResult.fail(CommonStatusEnum.VERIFICATION_CODE_ERROR.getCode(),CommonStatusEnum.VERIFICATION_CODE_ERROR.getValue());
+        }
+
+        //判断是否有用户，并进行对应的处理
+        System.out.println("判断是否有用户，并进行对应的处理");
+
+        //颁发令牌
+        System.out.println("颁发令牌");
+
+        //响应
+        TokenResponse tokenResponse = new TokenResponse();
+        tokenResponse.setToken("token value");
+        return ResponseResult.success(tokenResponse);
     }
 
 }
