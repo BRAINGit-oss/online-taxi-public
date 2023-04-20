9783f4d12f83901265e5a2b0cfe94a08ed42bf72
BRAINGit-oss <1834733454@qq.com>
2023-04-20 14:19:15 +0800

    乘客api，发送验证码服务完成。

--- api-passenger/src/main/java/com/brain/apipassenger/controller/VerificationCodeController.java
+++ api-passenger/src/main/java/com/brain/apipassenger/controller/VerificationCodeController.java
@@ -2,6 +2,7 @@ package com.brain.apipassenger.controller;
 
 import com.brain.apipassenger.request.VerificationCodeDTO;
 import com.brain.apipassenger.service.VerificationCodeService;
+import com.brain.internalcommon.dto.ResponseResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,9 +13,8 @@ public class VerificationCodeController {
    @Autowired
    private VerificationCodeService verificationCodeService;
     @GetMapping("/verification-code")
-    public String verificationCode(@RequestBody VerificationCodeDTO verificationCodeDTO){
+    public ResponseResult verificationCode(@RequestBody VerificationCodeDTO verificationCodeDTO){
         String passengerPhone = verificationCodeDTO.getPassengerPhone();
-        System.out.println("接受到的手机号为:"+passengerPhone);
         return verificationCodeService.generatorCode(passengerPhone);
     }
 }
--- internal-common/src/main/java/com/brain/internalcommon/dto/ResponseResult.java
+++ internal-common/src/main/java/com/brain/internalcommon/dto/ResponseResult.java
@@ -12,6 +12,15 @@ public class ResponseResult<T>{
     private T data;
 
     /**
+     * 成功相应的方法
+     * @return
+     * @param <T>
+     */
+    public static <T> ResponseResult success(){
+        return new ResponseResult().setCode(CommonStatusEnum.SUCCESS.getCode()).setData(CommonStatusEnum.SUCCESS.getValue());
+    }
+
+    /**
      * 成功响应的方法
      * @param data
      * @return
