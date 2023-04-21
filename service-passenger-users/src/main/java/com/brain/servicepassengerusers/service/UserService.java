95d7813f5ca540452700b6f308c8650a167cb128
BRAINGit-oss <1834733454@qq.com>
2023-04-21 14:47:25 +0800

    插入用户

--- service-passenger-users/src/main/java/com/brain/servicepassengerusers/service/UserService.java
+++ service-passenger-users/src/main/java/com/brain/servicepassengerusers/service/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -25,10 +26,22 @@ public class UserService {
         Map<String,Object> map = new HashMap<>();
         map.put("passenger_phone",passengerPhone);
         List<PassengerUser> passengerUserList = passengerUserMapper.selectByMap(map);
-        System.out.println(passengerUserList.size() == 0 ? "无记录":passengerUserList.get(0).getPassengerName());
-
 
         //判断用户信息是否存在
+        if(passengerUserList.size()==0){
+            //如果不存在，插入用户信息
+            PassengerUser passengerUser = new PassengerUser();
+            passengerUser.setPassengerName("张三");
+            passengerUser.setPassengerGender((byte) 0);
+            passengerUser.setPassengerPhone(passengerPhone);
+            passengerUser.setState((byte)0);
+
+            LocalDateTime now = LocalDateTime.now();
+            passengerUser.setGmtCreate(now);
+            passengerUser.setGmtModified(now);
+
+            passengerUserMapper.insert(passengerUser);
+        }
 
         //如果不存在，插入用户信息
 
